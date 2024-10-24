50ce0c065565fea61cc0dcd669da1c37990506e1
Enrique <enrmarore@alu.edu.gva.es>
2024-10-24 10:40:14 +0200

    24/10/2024

--- TEMA2/src/Ejercicio6/LecturaMenu.java
+++ TEMA2/src/Ejercicio6/LecturaMenu.java
@@ -10,46 +10,48 @@ public class LecturaMenu extends Thread{
             String linea;
             BufferedReader br = new BufferedReader(new FileReader("menu.txt"));
 
+            BufferedWriter bw1 = new BufferedWriter(new FileWriter("primeros.txt"));
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter("segundos.txt"));
+            BufferedWriter bw3 = new BufferedWriter(new FileWriter("postres.txt"));
+
             while ((linea = br.readLine()) != null){
+
                 if (linea.startsWith("1") && getName().equals("hilo1")){
+
                     if (!linea.contains("PRIMEROS")){
                         String[] lineaFormateada = linea.split("-");
                         System.out.println("El " + getName() + " escribe..." + lineaFormateada[1]);
 
-                        BufferedWriter bw = new BufferedWriter(new FileWriter("primeros.txt", true));
-                        bw.write(lineaFormateada[1] + "\n");
-                        bw.flush();
-                        bw.close();
+                        bw1.write(lineaFormateada[1] + "\n");
                     }
                 }
 
+
                 if (linea.startsWith("2") && getName().equals("hilo2")){
+
                     if (!linea.contains("SEGUNDOS")){
                         String[] lineaFormateada = linea.split("-");
                         System.out.println("El " + getName() + " escribe..." + lineaFormateada[1]);
 
-                        BufferedWriter bw = new BufferedWriter(new FileWriter("segundos.txt", true));
-                        bw.write(lineaFormateada[1] + "\n");
-                        bw.flush();
-                        bw.close();
+                        bw2.write(lineaFormateada[1] + "\n");
                     }
                 }
 
+
                 if (linea.startsWith("3") && getName().equals("hilo3")){
+
                     if (!linea.contains("POSTRES")){
                         String[] lineaFormateada = linea.split("-");
                         System.out.println("El " + getName() + " escribe..." + lineaFormateada[1]);
 
-                        BufferedWriter bw = new BufferedWriter(new FileWriter("postres.txt", true));
-                        bw.write(lineaFormateada[1] + "\n");
-                        bw.flush();
-                        bw.close();
+                        bw3.write(lineaFormateada[1] + "\n");
                     }
                 }
             }
+            bw1.close();
+            bw2.close();
+            bw3.close();
             br.close();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
